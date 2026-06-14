8de968b95f339b8c0b19ccb12baac058ae9eef62
nickpedro <43183915+nickpedro@users.noreply.github.com>
2026-06-14 12:14:48 -0300

    chore: initial project setup

---
+++ backend/MyFlix.Api/MyFlix.Api/Models/MovieStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace MyFlix.Api.Models
+{
+    // Esse enum representa o status de um filme, "Para Assistir" ou "Assistido".
+    public enum MovieStatus
+    {
+        ToWatch = 1,
+        Watched = 2
+    }
+}
