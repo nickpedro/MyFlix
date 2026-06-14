05900be6415526dd7792f0c57e1083a057a1873d
nickpedro <43183915+nickpedro@users.noreply.github.com>
2026-06-14 13:46:20 -0300

    feat: configure sqlite database and entity framework

---
+++ backend/MyFlix.Api/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyFlix.Api.Models;
+
+namespace MyFlix.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Movie> Movies { get; set; }
+    
+    }
+}
